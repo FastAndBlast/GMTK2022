9c5fa283d869e43a3140d3e69c0b8001291d3071
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2022-07-16 20:05:44 +1000

    Room connection system

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -24,40 +24,47 @@ public class Object
 
 public class Room : MonoBehaviour
 {
-    public Room test;
     public int width;
     public int height;
 
     private List<Object> contents;
 
-    private Cell[,] roomGrid;
+    public Cell[,] roomGrid;
 
     public Vector3Int originPos;
     public Vector3Int topRight;
 
-    private void Start()
+    private void Awake()
     {
         roomGrid = new Cell[width, height];
         originPos = Vector3Int.FloorToInt(gameObject.transform.position);
         topRight = originPos + width * Vector3Int.right + height * Vector3Int.forward;
         MakeCells();
-        JoinRooms(test);
+        RoomManager.instance.rooms.Add(this);
+    }
+
+    private void Start()
+    {
     }
 
     private void MakeCells()
     {
-        Cell first = new Cell();
-        first.position = originPos;
-        roomGrid[0, 0] = first;
-        Cell last = first;
-        for (int i = 1; i < width; i++)
+        Cell last = null;
+        for (int i = 0; i < width; i++)
         {
             // Create a cell moving to the right
             Cell newCell = new Cell();
-            newCell.position = last.position + Vector3.right;
             roomGrid[i, 0] = newCell;
-            last.neighbors[(int)direction.right] = newCell;
-            newCell.neighbors[(int)direction.left] = newCell;
+            if (i != 0)
+            {
+                newCell.position = last.position + Vector3.right;
+                last.neighbors[(int)direction.right] = newCell;
+                newCell.neighbors[(int)direction.left] = newCell;
+            }
+            else
+            {
+                newCell.position = originPos;
+            }
 
             // Extend that cells column upwards
             last = newCell;
@@ -77,12 +84,12 @@ public class Room : MonoBehaviour
     private void OnDrawGizmos()
     {
         originPos = Vector3Int.FloorToInt(gameObject.transform.position);
-        Vector3Int centre = new Vector3Int(originPos.x +  width / 2, 1, originPos.z + height / 2);
+        Vector3 centre = new Vector3(originPos.x + (float)width / 2, 1, originPos.z + (float)height / 2);
         Vector3Int size = new Vector3Int(width, 2, height);
         Gizmos.DrawWireCube(centre, size);
     }
 
-    private void JoinRooms(Room other)
+    public void JoinRooms(Room other)
     // We assume that the rooms don't overlap
     {
         // Rooms connect right and up, connections on the other sides are
@@ -96,87 +103,58 @@ public class Room : MonoBehaviour
             return;
         }
 
-        // Vertical
-        int start = other.originPos.x - originPos.x;
-        int end = other.topRight.x - originPos.x;
-        
-        // There is no overlap
-        if (end < 0 || start > width)
+        // We check if the edges are along the same line
+        if (other.originPos.z == topRight.z)
         {
-            return;
-        }
-
-        List<int> ours = new List<int>();
-        List<int> thiers = new List<int>();
-        for (int i = 0; i < width; i++)
-        {
-            if (start <= i && end <= i)
+            // Horizontal
+            int start = other.originPos.x - originPos.x;
+            int end = other.topRight.x - originPos.x - 1;
+            
+            // There is no overlap
+            if (end < 0 || start > height)
             {
-                ours.Add(i);
-                thiers.Add(i - start);
-                Debug.Log(i + " " + (i - start));
+                return;
+            }
+
+            //Debug.Log(gameObject.name + " [" + start + "," + end + "]");
+            //Debug.Log("[" + 0 + "," + (width - 1) + "]");
+            for (int i = 0; i < width; i++)
+            {
+                if (start <= i && i <= end)
+                {
+                    //Debug.Log(i + " " + (i - start));
+                    Cell ours = roomGrid[i, height - 1];
+                    Cell theirs = other.roomGrid[i - start, 0];
+                    ours.neighbors[(int)direction.right] = theirs;
+                    theirs.neighbors[(int)direction.left] = ours;
+                }
+            }
+        }
+        else if (other.originPos.x == topRight.x)
+        {
+            // Vertical
+            int start = other.originPos.z - originPos.z;
+            int end = other.topRight.z - originPos.z - 1;
+            
+            // There is no overlap
+            if (end < 0 || start > width)
+            {
+                return;
+            }
+
+            // Debug.Log(gameObject.name + " [" + start + "," + end + "]");
+            // Debug.Log("[" + 0 + "," + (height - 1) + "]");
+            for (int i = 0; i < height; i++)
+            {
+                if (start <= i && i <= end)
+                {
+                    // Debug.Log(i + " " + (i - start));
+                    Cell ours = roomGrid[width - 1, i];
+                    Cell theirs = other.roomGrid[0, i - start];
+                    ours.neighbors[(int)direction.right] = theirs;
+                    theirs.neighbors[(int)direction.left] = ours;
+                }
             }
         }
     }
-
-    // private void JoinRooms(Room other)
-    // // We assume that the rooms don't overlap
-    // {
-    //     // Rooms connect right and up, connections on the other sides are
-    //     // achieved by calling this on other one
-
-    //     int vertOverlap = -1;
-    //     int horiOverlap = -1;
-
-    //     // First check if there are any possible intersections
-    //     Vector3Int topRight = originPos + width * Vector3Int.right + height * Vector3Int.forward;
-
-    //     if (topRight == other.originPos)
-    //     {
-    //         // Diagonal rooms can't connect
-    //         return;
-    //     }
-
-    //     if (topRight.x <= other.originPos.x && originPos.x >= other.originPos.x)
-    //     {
-    //         horiOverlap = topRight.x - other.originPos.x;
-    //     }
-    //     if (topRight.z <= other.originPos.z && originPos.z >= other.originPos.z)
-    //     {
-    //         vertOverlap = topRight.z - other.originPos.z;
-    //     }
-
-    //     // The two have no connection
-    //     if (vertOverlap < 0 || horiOverlap < 0)
-    //     {
-    //         return;
-    //     }
-
-
-    //     if (vertOverlap > 0 && horiOverlap > 0)
-    //     {
-    //         Debug.LogError("Two rooms are overlapping, this is not allowed");
-    //         return;
-    //     }
-
-    //     if (vertOverlap == 0)
-    //     {
-    //         // The connection is along the top edge
-
-    //         // Account for the other object not reaching the top right
-    //         int stop = Mathf.Min(horiOverlap, other.height);
-    //         //for (int i = topRight.z - horiOverlap; i < topRight.z - stop; i++)
-    //         for(int i = 0; i < stop; i++)
-    //         {
-    //             int index = topRight.z - horiOverlap + i;
-    //             Cell ours = roomGrid[width - 1, index];
-    //             Cell thiers = other.roomGrid[0, i];
-
-    //         }
-    //     }
-    //     else
-    //     {
-    //         // The connection is along the right edge
-    //     }
-    // }
 }
