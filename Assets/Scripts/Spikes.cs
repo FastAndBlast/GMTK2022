9e58ccd041ef97b8921e3aa0f7c1e18c6498a5cd
Michael Kennedy <michael.kennedy@thedropbears.org.au>
2022-07-17 18:16:32 +1000

    Spikes and delete duplicate scripts folder

---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    private Vector2 direction;
+    Cell cell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float angle = transform.eulerAngles.y;
+        direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+    }
+
+    // Update is called once per frame
+    public void ActionTick()
+    {
+        for (int i = 0; i < GameManager.entities.Count; i++)
+        {
+            Entity entity = GameManager.entities[i];
+            if (entity.currentCell == cell)
+            {
+                entity.GetHit(direction, 1);
+            }
+        }
+    }
+}
